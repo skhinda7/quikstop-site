f78c2b350675b1a028927846c6015d1e847c7f96
Simardeep <skhinda7@gmail.com>
2023-07-19 14:37:16 -0500

    Completed dropdown for small window

--- function.js
+++ function.js
@@ -17,3 +17,13 @@ function showSlides() {
     dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 3500);
 }
+
+window.onload = function () {
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle("is-active");
+        mobile_menu.classList.toggle("is-active");
+    })
+}
