c67362f61a151cb4d8325a6466a80936d3116872
Simardeep <skhinda7@gmail.com>
2023-07-19 20:47:56 -0500

    fixed dropdown

--- function.js
+++ function.js
@@ -18,12 +18,9 @@ function showSlides() {
     setTimeout(showSlides, 3500);
 }
 
-window.onload = function () {
-    const menu_btn = document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav');
 
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle("is-active");
-        mobile_menu.classList.toggle("is-active");
-    })
-}
+// Event Handlers
+
+document.querySelector("#checkbox").addEventListener("change", function () {
+    document.querySelector('.dropdown-menu').classList.toggle('open');
+});
